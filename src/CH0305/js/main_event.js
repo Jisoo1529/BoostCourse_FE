831ccc4209fcfdc94eb02d0f2f2a698074b2bbb7
jisoo1529 <jsjwoo0512@gmail.com>
2019-08-26 11:44:05 +0900

    Web UI

--- src/CH0305/js/main_event.js
+++ src/CH0305/js/main_event.js
@@ -1,8 +1,28 @@
+var ul = document.querySelector("ul");
 var log = document.querySelector(".log");
-var lists = document.querySelectorAll("ul> li");
+
+ul.addEventListener("click",function(evt){
+    //target이 img면 src를 추출하여 출력
+    var target = evt.target;
+    if(evt.target.tagName ==="IMG"){
+        log.innerHTML = "IMG URL= "+target.src;
+    } else if (target.tagName ==="LI"){
+        log.innerHTML = "IMG URL ="+target.firstElementChild.src;
+    }
+})
+
+/*for문 실행
+var lists = document.querySelectorAll("ul > li");
 
 for(var i =0, len = lists.length; i < lists.length; i++) {
     lists[i].addEventListener("click", function(evt){
-        console.log(evt.curentTarget);
+        log.innerHTML = "IMG URL IS" +evt.currentTarget.firstElementChild.src; //currentTarget = li
     });
-}
+}*/
+
+/*img tag event bubbling
+var img = document.querySelector("img");
+
+img.addEventListener("click",function(){
+    console.log("Img is clicked");
+})  */
